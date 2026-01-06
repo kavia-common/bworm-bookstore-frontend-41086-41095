34366c11beaa76e44247f9c5e70bdad740d90ecf
Kavia AI <production@kavia.ai>
2026-01-06 08:34:42 +0000

    Checkpoint 2 - cg36abaac0

--- .init/.linter.sh
+++ .init/.linter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/kavia/workspace/code-generation/bworm-bookstore-frontend-41086-41095/frontend_bworm
+cd /home/kavia/workspace/code-generation/copy-of-bworm-bookstore-frontend-bworm-bookstore-frontend-41086-41095-41174/frontend_bworm
 npm run build
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
